7e821e7f50eee63e3c27d0011a3835d06cde73d1
jandromolina <alejandro.molina@estudiants.urv.cat>
2025-10-29 17:56:29 +0100

    Sube la nueva versión PokeApp sustituyendo lo anterior

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 fetch('header.html')
       .then(response => response.text())
       .then(data => {
-        document.getElementById('site-header').innerHTML = data;
+        document.getElementById('topbar').innerHTML = data;
       })
       .catch(error => console.error('Error loading header:', error));
